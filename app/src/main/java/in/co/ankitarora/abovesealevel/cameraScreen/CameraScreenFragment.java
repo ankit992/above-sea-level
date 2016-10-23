92ab81c0861e875183c018d1f5d3c8e8f2aa1d0b
Ankit Arora <ankit.arora@theredpandas.com>
2016-10-23 14:40:03 +0300

    adding basic mvp and contract for camera

--- app/src/main/java/in/co/ankitarora/abovesealevel/cameraScreen/CameraScreenFragment.java
+++ app/src/main/java/in/co/ankitarora/abovesealevel/cameraScreen/CameraScreenFragment.java
@@ -7,19 +7,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import in.co.ankitarora.abovesealevel.Common.Base.BaseFragment;
+import in.co.ankitarora.abovesealevel.Common.CameraContract;
 import in.co.ankitarora.abovesealevel.R;
 
 /**
  * Written by Ankit Arora on 22/10/16.
  */
 
-public class CameraScreenFragment extends BaseFragment implements CameraScreenContract.view {
+public class CameraScreenFragment extends BaseFragment implements CameraScreenContract.view, CameraContract {
+    CameraScreenContract.listener listener;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View contentView= super.onCreateView(inflater, container, savedInstanceState);
         if(contentView==null)
             contentView=inflater.inflate(R.layout.camera_screen_fragment,container,false);
+        getListener();
         return contentView;
+    }
+
+    public void getListener() {
+        listener=new CameraScreenPresenter(this);
     }
 }
--- app/src/main/java/in/co/ankitarora/abovesealevel/cameraScreen/CameraScreenPresenter.java
+++ app/src/main/java/in/co/ankitarora/abovesealevel/cameraScreen/CameraScreenPresenter.java
@@ -4,5 +4,9 @@ package in.co.ankitarora.abovesealevel.cameraScreen;
  * Written by Ankit Arora on 22/10/16.
  */
 
-public class CameraScreenPresenter implements CameraScreenContract.listener {
+class CameraScreenPresenter implements CameraScreenContract.listener {
+    CameraScreenContract.view view;
+    CameraScreenPresenter(CameraScreenContract.view view) {
+        this.view=view;
+    }
 }
